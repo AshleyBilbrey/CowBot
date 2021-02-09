d12b1c9e43ff1955ce8e928005704ac1391b3c35
Zhekun Hu <zhekunhu@gmail.com>
2021-02-09 05:42:48 +0000

    experimental pet command

---
+++ commands/pet.js
@@ -0,0 +1,9 @@
+module.exports = {
+        name: "pet",
+        description: "Reacts when someone pets the bot",
+        execute(message) {
+            const Discord = require('discord.js');
+            message.channel.send("moooooooooo\n");
+        }
+    }
+    
